33d170018e5f0facb46ff6cb563d4565fc8a0f1e
janikp <485122@mail.muni.cz>
2020-03-09 11:15:30 +0100

    Create maven project with gitignore.

---
+++ src/main/java/cz/muni/fi/pa165/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}

639d82271f6cdc4e10a6624f12b3a2a3cefde753
Martin Páleník <xpalenik@fi.muni.cz>
2020-03-16 18:15:08 +0100

    Correction: Add Loan class
    Forgot to add the code.

--- src/main/java/cz/muni/fi/pa165/library/entities/Loan.java
+++ src/main/java/cz/muni/fi/pa165/library/entities/Loan.java
@@ -1,4 +1,31 @@
 package cz.muni.fi.pa165.library.entities;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+class Member{};
+
+/** @author Martin Páleník 359817 */
+
 public class Loan {
+    Long id;
+    LocalDateTime borrowedAt;
+    HashMap<Long, String> returnedBookCondition;
+
+    Member member;
+    List<Book> books;
+
+    public Loan(Member member, List<Book> books) {
+        this.member = member;
+
+        for (Book book: books){
+            this.books.add(book);
+            returnedBookCondition.put(book.getId(),null);
+        }
+    }
+
+    public void returnBook(Long id, String condition){
+        returnedBookCondition.put(id,condition);
+    }
 }

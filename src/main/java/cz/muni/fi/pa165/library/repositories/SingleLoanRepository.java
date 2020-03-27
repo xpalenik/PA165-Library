db143cbf2cb711f4d31378c7022a524c8c3c81e1
Martin Páleník <xpalenik@fi.muni.cz>
2020-03-27 14:16:16 +0100

    Add SingleLoanRepository
    Provides CRUD operations for SingleLoan.

---
+++ src/main/java/cz/muni/fi/pa165/library/repositories/SingleLoanRepository.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.library.repositories;
+
+import cz.muni.fi.pa165.library.entities.SingleLoan;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/** @author Martin Páleník 359817
+ *  based on http://zetcode.com/springboot/crudrepository/
+ */
+
+@Repository
+public interface SingleLoanRepository extends CrudRepository<SingleLoan, Long> {
+}

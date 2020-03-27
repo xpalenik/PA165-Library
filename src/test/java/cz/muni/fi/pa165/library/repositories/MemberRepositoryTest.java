992a813d8aa9493011cd01d3a17c06ed49b8d366
Martin Páleník <xpalenik@fi.muni.cz>
2020-03-27 23:22:15 +0100

    Correct multiple errors in MemberRepositoryTest
    Turned out the tests were written so horribly they were actually not even executed. Many, many errors in my code. It should be working fine now.

--- src/test/java/cz/muni/fi/pa165/library/repositories/MemberRepositoryTest.java
+++ src/test/java/cz/muni/fi/pa165/library/repositories/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package cz.muni.fi.pa165.library.repositories;
 
-import cz.muni.fi.pa165.library.entities.Book;
 import cz.muni.fi.pa165.library.entities.Member;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
@@ -12,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,104 +18,92 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /** @author Martin Páleník 359817 */
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class MemberRepositoryTest {
 
-    @RunWith(SpringRunner.class)
-    @DataJpaTest
-    public class MemberRepositoryIntegrationTest {
+    @Autowired
+    private TestEntityManager entityManager;
 
-        @Autowired
-        private TestEntityManager entityManager;
+    @Autowired
+    private MemberRepository memberRepository;
 
-        @Autowired
-        private MemberRepository memberRepository;
+    @Test
+    public void findById() {
+        // given
+        Member martin = new Member();
+        martin.setFirstName("Martin");
+        martin.setSurname("REDACTED");
+        martin.setLibrarian(false);
+        entityManager.persist(martin);
+        entityManager.flush();
 
-        @Test
-        public void whenFindById_thenReturnMember() {
-            // given
-            Member martin = new Member();
-            martin.setMemberId(1);
-            martin.setFirstName("Martin");
-            martin.setSurname("R E D A C T E D");
-            martin.setLibrarian(false);
-            entityManager.persist(martin);
-            entityManager.flush();
+        // when
+        Optional<Member> found = memberRepository.findById(martin.getMemberId());
 
-            // when
-            Optional<Member> found = memberRepository.findById(martin.getMemberId());
+        // then
+        assertThat(found.get().getMemberId())
+                .isEqualTo(martin.getMemberId());
+        assertThat(found.get().getFirstName())
+                .isEqualTo(martin.getFirstName());
+        assertThat(found.get().getSurname())
+                .isEqualTo(martin.getSurname());
+    }
 
-            // then
-            assertThat(found.get().getMemberId())
-                    .isEqualTo(martin.getMemberId());
-        }
+    @Test
+    public void findAll() {
+        // given
+        Member martin = new Member();
+        martin.setFirstName("Martin");
+        martin.setSurname("REDACTED");
+        martin.setLibrarian(false);
+        entityManager.persist(martin);
+        entityManager.flush();
 
-        @Test
-        public void findAll() {
-            // given
-            Member martin = new Member();
-            martin.setMemberId(1);
-            martin.setFirstName("Martin");
-            martin.setSurname("R E D A C T E D");
-            martin.setLibrarian(false);
-            entityManager.persist(martin);
-            entityManager.flush();
+        Member librarian = new Member();
+        librarian.setFirstName("Librarian");
+        librarian.setSurname("REDACTED");
+        librarian.setLibrarian(true);
+        entityManager.persist(librarian);
+        entityManager.flush();
 
-            Member librarian = new Member();
-            librarian.setMemberId(2);
-            librarian.setFirstName("Librarian");
-            librarian.setSurname("R E D A C T E D");
-            librarian.setLibrarian(true);
-            entityManager.persist(librarian);
-            entityManager.flush();
+        // see https://www.baeldung.com/java-iterable-to-collection
+        Iterable<Member> i = memberRepository.findAll();
+        List<Member> foundMembers = new ArrayList<Member>();
+        i.forEach(foundMembers::add);
 
-            // see https://www.baeldung.com/java-iterable-to-collection
-            Iterable<Member> i = memberRepository.findAll();
-            List<Member> foundMembers = new ArrayList<Member>();
-            i.forEach(foundMembers::add);
+        MatcherAssert.assertThat(foundMembers, CoreMatchers.hasItems(martin, librarian));
+    }
 
-            MatcherAssert.assertThat(foundMembers, CoreMatchers.hasItems(martin, librarian));
-        }
+    @Test
+    public void count() {
+        // given
+        Member martin = new Member();
+        entityManager.persist(martin);
+        entityManager.flush();
 
-        @Test
-        public void count() {
-            // given
-            Member martin = new Member();
-            martin.setMemberId(1);
-            martin.setFirstName("Martin");
-            martin.setSurname("R E D A C T E D");
-            martin.setLibrarian(false);
-            entityManager.persist(martin);
-            entityManager.flush();
+        Member librarian = new Member();
+        entityManager.persist(librarian);
+        entityManager.flush();
 
-            Member librarian = new Member();
-            librarian.setMemberId(2);
-            librarian.setFirstName("Librarian");
-            librarian.setSurname("R E D A C T E D");
-            librarian.setLibrarian(true);
-            entityManager.persist(librarian);
-            entityManager.flush();
+        assertThat(memberRepository.count())
+                .isEqualTo(Long.valueOf(2));
+    }
 
-            assertThat(memberRepository.count())
-                    .isEqualTo(Long.valueOf(2));
-        }
+    @Test
+    public void deleteById() {
+        // given
+        Member martin = new Member();
+        entityManager.persist(martin);
+        entityManager.flush();
 
-        @Test
-        public void deleteById() {
-            // given
-            Member martin = new Member();
-            martin.setMemberId(1);
-            entityManager.persist(martin);
-            entityManager.flush();
+        Member librarian = new Member();
+        entityManager.persist(librarian);
+        entityManager.flush();
 
-            Member librarian = new Member();
-            librarian.setMemberId(2);
-            entityManager.persist(librarian);
-            entityManager.flush();
+        memberRepository.deleteById(librarian.getMemberId());
 
-            memberRepository.deleteById(Long.valueOf(2));
-
-            assertThat(memberRepository.count())
-                    .isEqualTo(Long.valueOf(1));
-        }
+        assertThat(memberRepository.count())
+                .isEqualTo(Long.valueOf(1));
     }
 }

bb033d85fbf8471a93fcb38a73c274631216098d
Martin Páleník <xpalenik@fi.muni.cz>
2020-04-14 17:55:03 +0200

    Add facade for loans

---
+++ library-api/src/main/java/cz/muni/fi/pa165/library/facade/SingleLoanFacade.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pa165.library.facade;
+
+import cz.muni.fi.pa165.library.dto.BookDTO;
+import cz.muni.fi.pa165.library.dto.SingleLoanDTO;
+import cz.muni.fi.pa165.library.dto.SingleLoanDTO.UserDTO;
+
+import java.util.List;
+
+/** @author Martin Páleník 359817 */
+public interface SingleLoanFacade {
+
+    /** The time of loan will be set to now() */
+    public void borrowBook(BookDTO bookDto, UserDTO userDto);
+
+    /** The time of return will be set to now() */
+    public void returnBook(BookDTO bookDto, UserDTO userDto, String state);
+
+    /**
+     * For the given user return all his loans (current or past).
+     * Answers "what a member borrowed and when".
+     * @return list of loans for the given user
+     */
+    public List<SingleLoanDTO> getLoansForUser(UserDTO userDto);
+
+    /**
+     * For the given book return all its loans (current or past).
+     * Answers "who borrowed a certain book".
+     * Answers "what condition they returned the book in".
+     * @return list of loans for the given book
+     */
+    public List<SingleLoanDTO> getLoansForBook(BookDTO bookDto);
+
+    public List<SingleLoanDTO> getAllSingleLoans();
+    public SingleLoanDTO getSingleLoanById(long id);
+
+}
